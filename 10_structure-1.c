1ff0becd323ff8825eea6e9b17684375823c57ef
soyoungna <hos05360@naver.com>
2018-12-04 12:59:15 +0900

    10_structure-2

--- 10_structure-1.c
+++ 10_structure-1.c
@@ -7,6 +7,7 @@ int main(int argc, char *argv[]) {
 	
 	FILE* fp;
 	char name[100];
+	char word[100];
 	
 	printf("input a file name : ");
 	scanf("%s",&name);
@@ -26,3 +27,58 @@ int main(int argc, char *argv[]) {
 
 	return 0;
 }
+
+int is_whitespace(char c)
+{
+	if( c==' '||
+	    c=='\t'||
+		c=='\n'||
+		c=='\r'||
+		c=='(' ||
+		c==')')
+		
+		return 1;
+		
+		return 0;
+}
+
+int fget_word(FILE* fp, char* word){
+	char c;
+	int cnt;
+	
+	
+	       //word processing
+	while((c=fgetc(fp)) != EOF){ //1 문자 받음.
+	
+	    if(is_whitespace(c) == 0){ //유의미한 숫자이면 나감
+			
+		           
+				   break; 
+     	} 
+	}       
+
+
+    if(c == EOF) //최초 읽은 문자가 마지막 일때 
+    {
+    	return 0;
+	}
+	//단어 한글자씩 읽 
+	cnt = 0;
+	word[cnt++] = c;
+	word[cnt] = '\0';
+	
+	while((word[cnt] = fgetc(fp)) != EOF)
+	{
+		if(is_whitespace(word[cnt]) == 1)
+		{
+			word[cnt] = '\0';
+			break;
+		}
+		
+		cnt++;
+	}
+	
+	return cnt;
+}
+
+
